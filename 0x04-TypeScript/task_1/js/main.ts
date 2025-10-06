282960b57351bd58574e5904bfd706a7285af843
jabezdubale <jabezdubale6@gmail.com>
2025-10-06 22:47:17 +0300

    3. Printing teachers

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,3 +10,10 @@ interface Teacher {
 interface Director extends Teacher {
   numberOfReports: number;
 }
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
